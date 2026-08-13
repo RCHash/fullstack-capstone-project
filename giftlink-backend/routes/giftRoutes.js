4613a43afbea02984b89b2212a73e3958f3a1476
RCHash <94483717+RCHash@users.noreply.github.com>
2026-08-13 08:19:01 -0400

    Update giftRoutes.js

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,19 +1,19 @@
 router.get('/', async (req, res) => {
     try {
-        // Task 1: Connect to MongoDB and store connection to db constant
-        // const db = {{insert code here}}
+      // Task 1: Connect to MongoDB and store connection to db constant
+      const db = await connectToDatabase();
 
-        // Task 2: use the collection() method to retrieve the gift collection
-        // {{insert code here}}
+      // Task 2: use the collection() method to retrieve the gift collection
+      const collection = db.collection('gifts');
 
-        // Task 3: Fetch all gifts using the collection.find method. Chain with toArray method to convert to JSON array
-        // const gifts = {{insert code here}}
+      // Task 3: Fetch all gifts using the collection.find method. Chain with toArray method to convert to JSON array
+      const gifts = await collection.find({}).toArray();
 
-        // Task 4: return the gifts using the res.json method
-        res.json(/* {{insert code here}} */);
+      // Task 4: return the gifts using the res.json method
+      res.json(gifts);
     } catch (e) {
-        console.error('Error fetching gifts:', e);
-        res.status(500).send('Error fetching gifts');
+      console.error('Error fetching gifts:', e);
+      res.status(500).send('Error fetching gifts');
     }
 });
 
@@ -41,13 +41,11 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-
-
 // Add a new gift
 router.post('/', async (req, res, next) => {
     try {
         const db = await connectToDatabase();
-        const collection = db.collection("gifts");
+        const collection = db.collection('gifts');
         const gift = await collection.insertOne(req.body);
 
         res.status(201).json(gift.ops[0]);
